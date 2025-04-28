6479ef5433d0d4da4aed952e3bff2a1ef711976c
ELISEO JESÚS MONTENEGRO RIVERA <Jesus107925@gmail.com>
2025-04-27 20:40:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
         badges.style.opacity = 1;
     }, 500);
 });
+
+// Función para validar el login
+function validarLogin(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de manera convencional
+
+    const usuario = document.getElementById('usuario').value;
+    const clave = document.getElementById('clave').value;
+
+    if ((usuario === "71397305" && clave === "12345") || 
+        (usuario === "USER713973051" && clave === "12345")) {
+        document.getElementById('login-form').style.display = 'none'; // Oculta el formulario de login
+        document.getElementById('contenido').style.display = 'block'; // Muestra el contenido protegido
+    } else {
+        alert("Usuario o contraseña incorrectos.");
+    }
+}
